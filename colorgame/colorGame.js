213369ae397d4a1b1b0b1c3e764900d6452d26d1
wickinot <27719504+wickinot@users.noreply.github.com>
2018-08-16 13:36:22 +1000

    Generate random colors using given colors array

--- colorgame/colorGame.js
+++ colorgame/colorGame.js
@@ -8,25 +8,43 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
 	// add initial colors to squares
-	squares[i].style.background = colors[i];
+	squares[i].style.backgroundColor = colors[i];
 
 	//add click listeners to squares
 	squares[i].addEventListener("click", function() {
 		//grab color of clicked squares
-		var clickedColor = this.style.background;
+		var clickedColor = this.style.backgroundColor;
 		//compare color to pickedColor
 		if(clickedColor === pickedColor) {
-			alert("Correct!");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		} else {
-			alert("WRONG!!!");
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
 }
 
+function changeColors(color){ 
+//loop through all squares
+	for(i = 0; i < squares.length; i++){
+// change each color to match given color
+	squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	// pick a random color
+	//math.random will pick a number between 0 & 1(not including 1) and multiply it by length of array.
+	//math.floor takes off the decimal to give you a whole no.
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
